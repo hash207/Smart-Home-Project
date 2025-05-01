abd9bce83376af45f5f599e44e532c5c47fc44b3
Hashem Alsharif <hashemalshar20.7@gmail.com>
2025-05-01 12:45:47 +0000

    Remove Replit Auth
    Delete the code that is supposed to link the app to replit authentication

--- Web app/main.py
+++ Web app/main.py
@@ -1,17 +1,7 @@
 import paho.mqtt.client as mqtt
-from flask import Flask, render_template, redirect, url_for, request
-from functools import wraps
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
-
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        user_id = request.headers.get('X-Replit-User-Id')
-        if not user_id:
-            return redirect('/__replit_auth_handle')
-        return f(*args, **kwargs)
-    return decorated_function
 Broker = "broker.emqx.io"
 client = mqtt.Client()
 
@@ -28,18 +18,15 @@ client.subscribe("HashLAP")
 client.loop_start()  # Start the MQTT loop in a separate thread
 
 @app.route("/room1/<string:led>")
-@login_required
 def toggle(led):
     client.publish("HashESP1", f"{led}")
     return redirect(url_for('room_1'))
 
 @app.route("/room1")
-@login_required
 def room_1():
     return render_template("room1.html", leds=["Main led", "Secondary LED", "Third LED"])
 
 @app.route("/")
-@login_required
 def home():
     return render_template("home.html")
 
